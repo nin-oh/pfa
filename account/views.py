08f84fe3e93a71422fcf4aec56518fecabddfb51
nin-oh <obadaoui5@gmail.com>
2023-05-21 23:51:13 +0100

    version 3

--- account/views.py
+++ account/views.py
@@ -13,7 +13,7 @@ def registration_view(request):
 			raw_password = form.cleaned_data.get('password1')
 			account = authenticate(email=email, password=raw_password)
 			login(request, account)
-			return redirect('home')
+			return redirect('page_acc')
 		else:
 			context['registration_form'] = form
 
--- cours/views.py
+++ cours/views.py
@@ -1,17 +1,16 @@
 
 from django.http import HttpResponseRedirect
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render ,redirect , get_object_or_404
 from cours.models import cours
 from cours.forms import CreateBlogPostForm
 from account.models import Account
 from operator import attrgetter
 
-from cours.models import cours
 
 def cours(request):
     return render(request, 'cours/cours.html')
 
-from django.shortcuts import render, redirect
+
 
 
 
@@ -40,15 +39,14 @@ def create_cours_view(request):
 def detail_cours_view(request, slug):
 	
 	context = {}
-	cours = get_object_or_404(cours, slug=slug)
-	context['cours'] = cours
+	cour_s = get_object_or_404(cours, slug=slug)
+	context['cour_s'] = cour_s
 
 	return render(request, 'cours/detail.html', context)
 
 def home_screen_view(request):
 	
-	context = {}
-	cours = sorted(cours.objects.all(), key=attrgetter('date_updated'), reverse=True)
-	context['cours'] = cours
-
-	return render(request, "cours/cours.html", context)
+    context = {}
+    cour_s = sorted(cours.objects.all(), key=attrgetter('date_updated'), reverse=True)
+    context['cour_s'] = cour_s
+    return render(request, "cours/cours.html", context)
